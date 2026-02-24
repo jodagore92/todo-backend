c59db7224bd0ae54179460bcf3b93783adca376e
Jose David Gomez <jodagore92@gmail.com>
2026-02-23 22:34:40 -0500

    CORs

--- app/main.py
+++ app/main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
 from app.api.v1 import api_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="ToDo API", redirect_slashes=True)
+
+origins = [
+    "http://localhost:3000",   # React
+    "http://localhost:5173",   # Vite
+    "http://localhost:4200",   # Angular
+    "http://localhost:80",   # Postman o navegador
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api_router)
     
